2b85dea05de2404d57f8cc05205c566d7335fe07
JuniorWolfgamingDE <JuniorWMG@lolcatz.de>
2025-09-15 23:17:26 +0200

    Improve config logic

--- src/main/java/net/juniorwmg/opensmashbats/config/ConfigManager.java
+++ src/main/java/net/juniorwmg/opensmashbats/config/ConfigManager.java
@@ -10,16 +10,10 @@ public class ConfigManager {
     public static boolean allowFutureMCCompat;
     public static Configuration config;
 
-    // TODO: improve this class! Duplicated code.
-
     public static void OpenSmashBatsConfig(FMLInitializationEvent event) {
         config = new Configuration(new File("config/opensmashbats.cfg"));
         config.load();
-
-        allowRidingFlyingMobs = config.getBoolean("Allow riding flying mobs", "Main", false, "Set to true to allow riding flying mobs. Hostile mobs like Ghasts may not like this. Use at your own risk.");
-        allowFutureMCCompat = config.getBoolean("Allow Future MC compat", "Main", true, "Set to false to disable Future MC compatibility. This removes the ability to craft the Netherite Smash Bat if Future MC is loaded. Does nothing if Future MC is not loaded.");
-        config.getCategory("Main").get("Allow Future MC compat").setRequiresMcRestart(true);
-
+        syncConfig();
         if (config.hasChanged()) {
             config.save();
         }
@@ -29,7 +23,6 @@ public class ConfigManager {
         allowRidingFlyingMobs = config.getBoolean("Allow riding flying mobs", "Main", false, "Set to true to allow riding flying mobs. Hostile mobs like Ghasts may not like this. Use at your own risk.");
         allowFutureMCCompat = config.getBoolean("Allow Future MC compat", "Main", true, "Set to false to disable Future MC compatibility. This removes the ability to craft the Netherite Smash Bat if Future MC is loaded. Does nothing if Future MC is not loaded.");
         config.getCategory("Main").get("Allow Future MC compat").setRequiresMcRestart(true);
-
         if (config.hasChanged()) {
             config.save();
         }
